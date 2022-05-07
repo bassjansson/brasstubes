461ec33b57ce20238db337b652e5a814eeb85057
Bass Jansson <tech@bassjansson.com>
2022-05-07 10:01:53 +0200

    Updates defines to work with 5 slaves total.

--- main/Defines.h
+++ main/Defines.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 const char *WIFI_SSID = "BrassTubesAP";
-const char *WIFI_PASS = "123456789";
+const char *WIFI_PASS = "KoenDeGrootBassJansson";
 
 const char *MIDI_FILE_NAME = "MIDITEST.MID";
 
@@ -12,7 +12,7 @@ const char *MIDI_FILE_NAME = "MIDITEST.MID";
 
 #define TOF_DELAY 100 // ms
 
-#define NUMBER_OF_DEVICES 4
+#define NUMBER_OF_DEVICES 6 // 1 master + 5 slaves
 
 // MAC addresses
 const uint8_t DEVICE_MAC_ADDRESSES[NUMBER_OF_DEVICES][6] = {
@@ -20,22 +20,28 @@ const uint8_t DEVICE_MAC_ADDRESSES[NUMBER_OF_DEVICES][6] = {
     {0x84, 0xCC, 0xA8, 0x61, 0x26, 0xEC}, // Slave 1 - 84:CC:A8:61:26:EC
     {0x84, 0xCC, 0xA8, 0x61, 0x2D, 0xC4}, // Slave 2 - 84:CC:A8:61:2D:C4
     {0xC4, 0x4F, 0x33, 0x56, 0x56, 0x5D}, // Slave 3 - C4:4F:33:56:56:5D
+    {0xC4, 0x4F, 0x33, 0x56, 0x56, 0x5E}, // Slave 4 - C4:4F:33:56:56:5E (TODO)
+    {0xC4, 0x4F, 0x33, 0x56, 0x56, 0x5F}, // Slave 5 - C4:4F:33:56:56:5F (TODO)
 };
 
 // Notes
 const uint8_t DEVICE_NOTES[NUMBER_OF_DEVICES][2] = {
     {0, 0},   // Master
-    {48, 50}, // Slave 1
-    {52, 53}, // Slave 2
-    {55, 57}, // Slave 3
+    {48, 50}, // Slave 1 (c, d)
+    {52, 53}, // Slave 2 (e, f)
+    {55, 57}, // Slave 3 (g, a)
+    {59, 60}, // Slave 4 (b, c)
+    {62, 64}, // Slave 5 (d, e)
 };
 
 // Synchronisation times (ms)
 const unsigned long DEVICE_SYNC_TIMES[NUMBER_OF_DEVICES] = {
-    0,   // Master
-    200, // Slave 1
-    400, // Slave 2
-    600, // Slave 3
+    0,    // Master
+    200,  // Slave 1
+    400,  // Slave 2
+    600,  // Slave 3
+    800,  // Slave 4
+    1000, // Slave 5
 };
 
 // Data structs
